ebbe119f99cad088e4d70d130f37c17e3a3daa92
Max Lettenberger <maxlettenberger@gmail.com>
2022-04-29 14:54:05 -0400

    add all components

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import './App.css';
 import NavBar from './components/NavBar/NavBar';
 import Main from './components/Main/Main';
 import About from './components/About/About';
+import Projects from './components/Projects/Projects';
+import Contact from './components/Contact/Contact';
+import Footer from './components/Footer/Footer';
 
 
 function App() {
@@ -10,6 +13,9 @@ function App() {
       <NavBar />
       <Main />
       <About />
+      <Projects />
+      <Contact />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Projects/Projects.js
@@ -0,0 +1,96 @@
+function Projects() {
+  return (
+    <div className="projects" id="projects">
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-sm-12 col-md-12 text-center">
+            <div className="projects-header">
+              <h1>Projects and Portfolio</h1>
+            </div>
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="col-sm-12 col-md-12 text-center">
+            <div className="projects-header-quote">
+              <h3>Sharing my endeavors and passions...</h3>
+            </div>
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="col-sm-4 col-md-4 text-center">
+            <div className="luvtalk-project">
+              <h3>LUV TALK Website</h3>
+              <p>
+                Website, created during my first internship, which utilized
+                HTML, CSS, PHP, Ajax, Javascript/jQuery, and Wordpress.{" "}
+              </p>
+              <div className="row">
+                <div className="col-sm-4 col-md-4 col-sm-offset-4 col-md-offset-4">
+                  <div className="project-more-info">
+                    <a className="btn btn-default btn-border" href="#">
+                      More
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="col-sm-4 col-md-4 text-center">
+            <div className="personal-website-project">
+              <h3>Personal Website</h3>
+              <p>
+                Enjoyable side project that was created to experiment with more
+                HTML and CSS, but also provided an outlet to showcase my
+                abilities and interests.
+              </p>
+              <div className="row">
+                <div className="col-sm-4 col-md-4 col-sm-offset-4 col-md-offset-4">
+                  <div className="project-more-info">
+                    <a className="btn btn-default btn-border" href="#">
+                      More
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="col-sm-4 col-md-4 text-center">
+            <div className="strike-zone-project">
+              <h3>Strike Zone Analysis</h3>
+              <p>
+                Data analytics project completed during my time at the Illinois
+                Math and Science Academy which studied the baseball strike zone
+                based on the state of the game.
+              </p>
+              <div className="row">
+                <div className="col-sm-4 col-md-4 col-sm-offset-4 col-md-offset-4">
+                  <div className="project-more-info">
+                    <a className="btn btn-default btn-border" href="#">
+                      More
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="col-sm-2 col-md-2 col-sm-offset-5 col-md-offset-5">
+            <div className="see-more-button">
+              <a className="btn btn-default btn-border" href="#">
+                More Projects
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Projects;
