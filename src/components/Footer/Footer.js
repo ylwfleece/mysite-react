a5d5f4d068f276ef6f0cccbb4bb83ec8742748c7
Max Lettenberger <maxlettenberger@gmail.com>
2022-05-02 13:32:00 -0400

    add styling to footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,30 +1,32 @@
+import './Footer.css'
+
 function Footer() {
     return (
         <div className = "footer">
 			<div className = "container-fluid">
 				<div className = "row">
-					<div className = "media-links">
+					<div className = "media-links media-links-row">
 						<div className = "col-sm-1 col-md-1 col-sm-offset-4 col-md-offset-4">
 							<div className = "facebook">
-								<a href = "#"><img className = "img-responsive" src = "./images/facebook.svg" /></a>
+								<a href = "#"><img className = "img-responsive logo-social" src = "./images/facebook.svg" /></a>
 							</div>
 						</div>
 
 						<div className = "col-sm-1 col-md-1">
 							<div className = "linkedin">
-								<a href = "#"><img className = "img-responsive" src = "./images/linkedin.svg" /></a>
+								<a href = "#"><img className = "img-responsive logo-social" src = "./images/linkedin.svg" /></a>
 							</div>
 						</div>
 
 						<div className = "col-sm-1 col-md-1">
 							<div className = "pinterest">
-								<a href = "#"><img className = "img-responsive" src = "./images/pinterest.svg" /></a>
+								<a href = "#"><img className = "img-responsive logo-social" src = "./images/pinterest.svg" /></a>
 							</div>
 						</div>
 
 						<div className = "col-sm-1 col-md-1">
 							<div className = "email-icon">
-								<a href = "#"><img className = "img-responsive" src = "./images/email.svg" /></a>
+								<a href = "#"><img className = "img-responsive logo-social" src = "./images/email.svg" /></a>
 							</div>
 						</div>
 					</div>
