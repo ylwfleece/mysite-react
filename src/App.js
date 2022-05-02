e6ef3e6b28fc436cea864a39e6bd2d946a5e403d
Max Lettenberger <maxlettenberger@gmail.com>
2022-05-02 13:50:48 -0400

    refactor nav and foot

--- src/App.js
+++ src/App.js
@@ -10,56 +10,58 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 function App() {
   return (
     <BrowserRouter>
+      <NavBar />
       <Routes>
-        <Route path={'/'} element={<><NavBar /><Main /><About /><Projects /><Contact /><Footer /></>} />
-        <Route path={'/main'} element={<><NavBar /><Main /><About /><Projects /><Contact /><Footer /></>} />
-        <Route path={'/projects'} element={<><NavBar /><Projects /><Footer /></>} />
-        <Route path={'/contact'} element={<><NavBar /><Contact /><Footer /></>} />
-        <Route path={'/about'} element={<><NavBar /><About /><Footer /></>} />
-
+        <Route
+          path={"/"}
+          element={
+            <>
+              <Main />
+              <About />
+              <Projects />
+              <Contact />
+            </>
+          }
+        />
+        <Route
+          path={"/main"}
+          element={
+            <>
+              <Main />
+              <About />
+              <Projects />
+              <Contact />
+            </>
+          }
+        />
+        <Route
+          path={"/projects"}
+          element={
+            <>
+              <Projects />
+            </>
+          }
+        />
+        <Route
+          path={"/contact"}
+          element={
+            <>
+              <Contact />
+            </>
+          }
+        />
+        <Route
+          path={"/about"}
+          element={
+            <>
+              <About />
+            </>
+          }
+        />
       </Routes>
+      <Footer />
     </BrowserRouter>
   );
 }
-
-// function App() {
-//   return (
-//     <BrowserRouter>
-//       <Switch>
-//         <Route exact path={"/"}>
-//           <NavBar />
-//           <Main />
-//           <About />
-//           <Projects />
-//           <Contact />
-//           <Footer />
-//         </Route>
-//         <Route exact path={"/main"}>
-//           <NavBar />
-//           <Main />
-//           <About />
-//           <Projects />
-//           <Contact />
-//           <Footer />
-//         </Route>
-//         <Route exact path={"/about"}>
-//           <NavBar />
-//           <About />
-//           <Footer />
-//         </Route>
-//         <Route exact path={"/projects"}>
-//           <NavBar />
-//           <Projects />
-//           <Footer />
-//         </Route>
-//         <Route exact path={"/contact"}>
-//           <NavBar />
-//           <Contact />
-//           <Footer />
-//         </Route>
-//       </Switch>
-//     </BrowserRouter>
-//   );
-// }
 
 export default App;
